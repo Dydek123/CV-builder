917a8a19016ba165d67e0b41fb9ec3db5f6b3790
Dydek123 <mazurkiewicz707@gmail.com>
2021-03-20 11:40:20 +0100

    Create simple login panel

--- CV-creator/src/app/app.module.ts
+++ CV-creator/src/app/app.module.ts
@@ -7,13 +7,13 @@ import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/posts-create/post-create.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
