910f312fd2918cca6cf38fdceed1ffdb7c52faac
Dydek123 <mazurkiewicz707@gmail.com>
2021-04-21 11:47:39 +0200

    Update login component

--- CV-creator/src/app/login/login.component.ts
+++ CV-creator/src/app/login/login.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-
+export class LoginComponent{
+  login:string = '';
+  password:string = '';
+  message:string = '';
   constructor() { }
-
-  ngOnInit(): void {
+  onLogin() {
+    if (this.login === 'admin' && this.password==='admin') this.message='Succesfully logged';
+    else this.message='Incorrect data';
   }
-
 }
