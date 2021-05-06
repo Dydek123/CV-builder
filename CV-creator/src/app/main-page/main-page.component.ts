b119be83d35f0eb90c74f3691010e08edac4d84a
Dydek123 <mazurkiewicz707@gmail.com>
2021-05-06 10:34:08 +0200

    Change What clients says section view

--- CV-creator/src/app/main-page/main-page.component.ts
+++ CV-creator/src/app/main-page/main-page.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {DialogComponent} from "../profile/dialog/dialog.component";
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MainPageComponent {
+  button_value = 0;
+  opinions = [
+    {name:'Jan Kowalski', image:'example_photo.jpg', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat ultricies pharetra. Donec fermentum eget metus vitae finibus. Nulla imperdiet, tellus ac finibus placerat, neque dolor facilisis nisi, eu euismod metus lorem id neque. Aliquam non sapien at lacus euismod iaculis id consequat ipsum. Cras dapibus nisi vitae pretium iaculis.'},
+    {name:'Anna Kowalczyk', image:'example_photo.jpg',  text: 'xD.'},
+    {name:'Zdzisław Marecki', image:'example_photo.jpg', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat ultricies pharetra. Donec fermentum eget metus vitae finibus. Nulla imperdiet, tellus ac finibus placerat, neque dolor facilisis nisi, eu euismod metus lorem id neque. Aliquam non sapien at lacus euismod iaculis id consequat ipsum. Cras dapibus nisi vitae pretium iaculis.'},
+  ]
 }
